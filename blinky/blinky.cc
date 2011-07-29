e4bb366b3a1f5845cdd96bcb6e4c5b44cc325f55
David Hinkes <david.hinkes@gmail.com>
2011-07-29 15:45:11 -0700

    good

--- blinky/blinky.cc
+++ blinky/blinky.cc
@@ -1,3 +1,4 @@
+#include <string.h>
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -5,35 +6,26 @@
 void USART_Transmit(const char* data, unsigned int size);
 void USART_Init(uint32_t ubrr);
 
+static char msg[] = "Hello world!";
+
 int main() {
-  const unsigned int BAUD = 9600;
-  const unsigned int MYUBRR = F_CPU / 16 / BAUD - 1;
-  volatile float c;
-  //USART_Init((F_CPU/(9600*16UL)-1)-1);
-  USART_Init(115200);
-  //DDRB |= (1<<PB0);
+  USART_Init(9600);
+  DDRB |= (1<<PB0);
   while (1) {
-    //PORTB |= (1<<PB0);
-    USART_Transmit("hello\n", 7); 
+    PORTB |= (1<<PB0);
+    USART_Transmit(msg, strlen(msg)); 
     _delay_ms(1000);
-    /*for (int i = 0; i < 100; ++i) {
-      float a = 5.5;
-      float b = 2.2;
-      c = a*b;
-    }
-    */
-    //PORTB &= ~(1<<PB0);
+    PORTB &= ~(1<<PB0);
   }
   return 0;
 }
 
 void USART_Init(uint32_t baud) {
-  uint16_t ubrr = F_CPU / 16 / baud - 1;
+  uint16_t ubrr = F_CPU / (16 * baud) - 1;
   UBRR0H = (unsigned char) (ubrr>>8);
   UBRR0L = (unsigned char) (0xff & ubrr);
   UCSR0C = (3<<UCSZ00); // N81.
   UCSR0B = (1<<TXEN0); // Tx on.
-  UCSR0A = 0;
 }
 
 void USART_Transmit(char data) {
